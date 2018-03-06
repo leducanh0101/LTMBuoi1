a4dd5af82086f31db5cc71224f27b8fd2cd5af5b
Anh <biebo0101@gmail.com>
2018-03-06 17:29:18 +0700

    xong

---
+++ BT1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Net;
+
+namespace BT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strHostName = Dns.GetHostName();
+   Console.WriteLine("Host Name: " + strHostName);
+ 
+   // Find host by name
+   IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+ 
+   // Enumerate IP addresses
+   int nIP = 0;
+   foreach (IPAddress ipaddress in iphostentry.AddressList)
+   {
+       Console.WriteLine("IP #" + ++nIP + ": " +
+                         ipaddress.ToString());
+   }
+        }
+    }
+}
